b9230562469b2eb3efd6227404cc9af24cfa4277
Duluman Sebastian <53044869+Sebicaduluman1312@users.noreply.github.com>
2023-04-27 15:54:46 +0300

    Update Schelet

--- neuro-app/src/App.tsx
+++ neuro-app/src/App.tsx
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Apucati-va de treaba, ca nu mai aveti timp de pierdut!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-=======
 import React, { useState } from "react";
 import "./CSS/App.css";
 import "./CSS/AdminPage.css";
@@ -38,7 +14,6 @@ function App() {
       <Route path="Admin" element={<Admin />}></Route>
       <Route path="CreateAccount" element={<Create />}></Route>
     </Routes>
->>>>>>> Stashed changes
   );
 }
 
