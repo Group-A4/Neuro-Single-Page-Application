e9ea8c3bc77fddcd212c7289b297ae51d95e456e
anaungurean <anamariaungurean01@gmail.com>
2023-04-27 12:26:28 +0300

    Update App.tsx

--- neuro-app/src/App.tsx
+++ neuro-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Apucati-va de treaba, ca nu mai aveti timp de pierdut!
+          Hello World!
         </p>
         <a
           className="App-link"
