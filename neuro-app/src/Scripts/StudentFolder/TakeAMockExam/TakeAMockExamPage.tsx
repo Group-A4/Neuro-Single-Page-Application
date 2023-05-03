4719bdabac4ed36bf12eb9e37e3151b6be1a6340
Bodnar Florina-Alina <bodnarflorina55@gmail.com>
2023-05-03 23:04:49 +0300

    .

--- neuro-app/src/Scripts/StudentFolder/TakeAMockExam/TakeAMockExamPage.tsx
+++ neuro-app/src/Scripts/StudentFolder/TakeAMockExam/TakeAMockExamPage.tsx
@@ -2,9 +2,16 @@ import React from 'react'
 import styles from './MockExam.module.css';
 import Nav from '../NavBarStudent/Nav';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const Body: React.FC<{}> = () => {
+
+    const navigate = useNavigate();
+    const goToTakeExam = () => {
+        navigate('/QuestionMockExam'); 
+      };
+
     return (
         <>
             <div className={styles['body--container']}>
@@ -26,39 +33,31 @@ const Body: React.FC<{}> = () => {
                     <div className={styles['course-title']}>
                         Subject title 1
                     </div>
-                    <button> Start </button> 
+                    <button onClick={goToTakeExam}>Start</button>
                 </div>
                 <div className={styles['course-container']}>
                     <div className={styles['course-title']}>
                         Subject title 2
                     </div>
-                    <div className={styles['course-title']}>
-                                ana
-                    </div>
+                    <button onClick={goToTakeExam}>Start</button>
                 </div>
                 <div className={styles['course-container']}>
                     <div className={styles['course-title']}>
                         Subject title 3
                     </div>
-                    <div className={styles['course-title']}>
-                                ana
-                    </div>
+                    <button onClick={goToTakeExam}>Start</button>
                 </div>
                 <div className={styles['course-container']}>
                     <div className={styles['course-title']}>
                         Subject title 4
                     </div>
-                    <div className={styles['course-title']}>
-                                ana
-                    </div>
+                    <button onClick={goToTakeExam}>Start</button>
                 </div>
                 <div className={styles['course-container']}>
                     <div className={styles['course-title']}>
-                                ana
+                        Subject title 5
                     </div>
-                    <div className={styles['course-title']}>
-                                ana
-                    </div>
+                    <button onClick={goToTakeExam}>Start</button>
                 </div>
             </div> 
 
