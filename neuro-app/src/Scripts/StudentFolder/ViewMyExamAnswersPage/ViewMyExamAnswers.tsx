484a7327b70c035fe14ce28175bc6aee070b7200
gabrielachirila <gabrielachirila239@gmail.com>
2023-05-11 22:37:49 +0300

    .

--- neuro-app/src/Scripts/StudentFolder/ViewMyExamAnswersPage/ViewMyExamAnswers.tsx
+++ neuro-app/src/Scripts/StudentFolder/ViewMyExamAnswersPage/ViewMyExamAnswers.tsx
@@ -1,27 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Nav from '../NavBarStudent/Nav';
-import styles from './Body.module.css';
-import { Link } from 'react-router-dom';
-import Frame from '../Components/Frame';
+import styles from './ViewMyExamAnswers.module.css';
 import '../QuestionMockExamPage/QuestionMockExamPage';
+import Frame from "../Components/Frame";
 
 interface QuizQuestion {
+    id: number;
     idCourse: number;
     idProfessor: number;
     questionText: string;
-    answersQuestion?: QuizAnswer[]; // make answersQuestion optional
-    isMultipleChoice: boolean; // add a flag to indicate if the question is a multiple choice question
+    answersQuestion?: QuizAnswer[];
+    isMultipleChoice: boolean; 
 }
 
 interface QuizAnswer {
     idQuestion: number;
     answerText: string;
-    correct: boolean;
+    correct?: boolean;
+    chosen?: boolean;
 }
 
 const hardcodedQuestions: QuizQuestion[] = [
 
      {
+        id: 1,
         idCourse: 1,
         idProfessor: 1,
         questionText: "What is the formula for water?",
@@ -29,6 +31,7 @@ const hardcodedQuestions: QuizQuestion[] = [
         isMultipleChoice: false,
     },
     {
+        id: 2,
         idCourse: 1,
         idProfessor: 1,
         questionText: "What is the symbol for the element oxygen?",
@@ -36,6 +39,7 @@ const hardcodedQuestions: QuizQuestion[] = [
         isMultipleChoice: false,
     },
     {
+        id: 3,
         idCourse: 1,
         idProfessor: 1,
         questionText: "What is the capital of France?",
@@ -64,6 +68,7 @@ const hardcodedQuestions: QuizQuestion[] = [
         isMultipleChoice: true,
     },
     {
+        id: 4,
         idCourse: 1,
         idProfessor: 1,
         questionText: "What is the largest ocean in the world?",
@@ -91,67 +96,63 @@ const hardcodedQuestions: QuizQuestion[] = [
         ],
         isMultipleChoice: true,
     }
-   
 ];
 
+const userAnswers: QuizAnswer[] = [
+  { idQuestion: 1, answerText: "Response1" },
+  { idQuestion: 2, answerText: "Response2" },
+  { idQuestion: 3, answerText: "Paris", correct: true, chosen: true },
+  { idQuestion: 4, answerText: "Indian Ocean", correct: false, chosen: true },
+];
 
 const Body: React.FC<{}> = () => {
-  const [questions, setQuestions] = useState<QuizQuestion[]>(hardcodedQuestions);
+  const [questions, setQuestions] =
+    useState<QuizQuestion[]>(hardcodedQuestions);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-
-  const handleNextQuestion = () => {
-    setCurrentQuestionIndex(currentQuestionIndex + 1);
-  };
-
-  const handlePreviousQuestion = () => {
-    setCurrentQuestionIndex(currentQuestionIndex - 1);
-  };
-
-  const handleChoiceSelect = (choice: string) => {
-    // handle the user's choice here
-  };
-
-  const handleInputAnswer = (answer: string) => {
-    // handle the user's input answer here
-  };
-
-  function handleFinishMockExam() {
-    // handle the user's finish exam here
-  }
 
   return (
     <div className="body">
-      <div className={styles["body--title"]}>
-        Here are your answers to the exam
-      </div>
+      {questions.map((question, index) => {
+        return (
+          <div key={question.id} className="questionPart">
+            <div className="questionQuery">
+              <h1 className="question">
+                {index + 1}/{questions.length} {question.questionText}
+              </h1>
+            </div>
 
-      {questions.map((currentQuestion, index) => (
-        <div key={index} className="questionPart">
-          <div className="questionQuery">
-            <h1 className="question">
-              {index + 1}/{questions.length} {currentQuestion.questionText}
-            </h1>
-          </div>
-
-          {currentQuestion.isMultipleChoice ? (
-           <div className="questionAnswers">
-                <ul>
-                {currentQuestion.answersQuestion?.map((answer, index) => (
-                    <li key={index} className="choice">
-                    <label>
+            {question.isMultipleChoice ? (
+              <div className="questionAnswers">
+                {question.answersQuestion ? (
+                  <ul>
+                    {question.answersQuestion.map((answer, index) => (
+                      <li
+                        key={index}
+                        className={
+                          answer.correct
+                            ? styles.correctAnswers
+                            : styles.choices
+                        }
+                      >
                         {answer.answerText}
-                    </label>
-                    </li>
-                ))}
-                </ul>
-            </div>
-          ) : (
-            <div className="questionAnswers">
-            <p> Response </p> 
-            </div>
-          )}
-        </div>
-      ))}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  ""
+                )}
+                <p className={styles["yourAnswers"]}>
+                  Your answer/s: {userAnswers[index].answerText}
+                </p>
+              </div>
+            ) : (
+              <div className="questionAnswers">
+                <p>{userAnswers[index].answerText}</p>
+              </div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
