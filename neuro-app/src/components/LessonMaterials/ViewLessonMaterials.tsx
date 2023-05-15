1022a7e70cf45ccee20aceb1c452447ab366c784
AndreiGalan <andreigalan03@gmail.com>
2023-05-16 00:13:23 +0300

    Added Delete button + Edit button(not ended)

--- neuro-app/src/components/LessonMaterials/ViewLessonMaterials.tsx
+++ neuro-app/src/components/LessonMaterials/ViewLessonMaterials.tsx
@@ -2,46 +2,92 @@ import React, { useEffect, useState } from 'react';
 import styles from './Body.module.css';
 import Nav from '../nav/Nav';
 import { SERVER_ADDRESS } from '../../config/config';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+
 
 type Props = {
   id_course: string;
 };
 
 const useGetMaterials = (id_course: string) => {
-    const [materials, setMaterials] = useState<any[]>([]);
+  const [materials, setMaterials] = useState<any[]>([]);
 
-    useEffect(() => {
-      fetchMaterials(id_course);
-    }, [id_course]);
-  
-    const fetchMaterials = async (id_course: string) => {
+  const fetchMaterials = async (id_course: string) => {
+    try {
+      const response = await fetch(
+        SERVER_ADDRESS + `/materials/id_lecture=${id_course}`
+      );
+      const data = await response.json();
+      setMaterials(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMaterials(id_course);
+  }, [id_course]);
+
+  return { materials, fetchMaterials }; // Return both materials and fetchMaterials
+};
+
+const ViewLessonMaterials: React.FC<Props> = ({ id_course }) => {
+  const { materials, fetchMaterials } = useGetMaterials(id_course); // Destructure fetchMaterials
+
+  const handleDelete = async (materialId: string) => {
+    if (window.confirm('Are you sure you want to delete this material?')) {
       try {
-        const response = await fetch(SERVER_ADDRESS + `/materials/id_lecture=${id_course}`);
-        const data = await response.json();
-        setMaterials(data);
+        await fetch(
+          SERVER_ADDRESS + `/materials/${materialId}`,
+          { method: 'DELETE' }
+        );
+        // Refresh the materials after deleting
+        fetchMaterials(id_course);
       } catch (error) {
         console.error(error);
       }
-    };
-    return materials;
-}
+    }
+  };
 
-const ViewLessonMaterials: React.FC<Props> = ({ id_course }) => {
-  
-    const materials = useGetMaterials(id_course);
-    
   return (
     <>
-    <Nav />
+      <Nav />
       <body className={styles['body']}>
-        <div className={styles['body--text']}><p className={styles["p-style"]}>View <span className={styles['body--text2']}>lecture </span> materials</p></div>
+        <div className={styles['body--text']}>
+          <p className={styles['p-style']}>
+            View <span className={styles['body--text2']}>lecture </span>
+            materials
+          </p>
+        </div>
         <div className={styles['body--content']}>
-        <ol className={styles['scrollable-list']}>
+          <ol className={styles['scrollable-list']}>
             {materials.map((material) => (
-              <li key={material.id} className={styles["li-style"]}>
-                <a href="http://localhost:3000/ViewMaterial" target="_blank" rel="noopener noreferrer">
-                  {material.title}
-                </a>
+              <li key={material.id} className={styles['li-style']}>
+                <div className={styles['left-side']}>
+                  <a
+                    className={styles['a--style']}
+                    href="http://localhost:3000/ViewMaterial"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {material.title}
+                  </a>
+                </div>
+                <div className={styles['right-side']}>
+                <button
+                    className={styles['editButton']}
+                  >
+                    <FontAwesomeIcon icon={faEdit} className={styles['Icon']} /> Edit
+                  </button>
+                  <button
+                    className={styles['deleteButton']}
+                    onClick={() => handleDelete(material.id)}
+                  >
+                    <FontAwesomeIcon icon={faTrash} className={styles['Icon']} /> Delete
+                  </button>
+                </div>
               </li>
             ))}
           </ol>
@@ -50,6 +96,5 @@ const ViewLessonMaterials: React.FC<Props> = ({ id_course }) => {
     </>
   );
 };
-
 
 export default ViewLessonMaterials;
