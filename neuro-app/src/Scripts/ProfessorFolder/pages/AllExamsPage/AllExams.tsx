595ff5aa42155e3aefb67e913e6cc8d93dc519ac
DianaCarcea <dianacarcea5@gmail.com>
2023-05-22 14:13:46 +0300

    com

--- neuro-app/src/Scripts/ProfessorFolder/pages/AllExamsPage/AllExams.tsx
+++ neuro-app/src/Scripts/ProfessorFolder/pages/AllExamsPage/AllExams.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, } from 'react'
 import styles from './Body1.module.css'
 
 
@@ -9,13 +9,22 @@ import { Column, useTable } from 'react-table';
 import ButtonViewExam from '../../components/buttonViewExam/ButtonViewExam';
 import Nav from '../../components/nav/Nav';
 
+interface Course {
+    id: number,
+    title: string,
+    year: number
+    semester: number,
+    credits: number
+}
 
 interface UserData {
 
     subject_title: string;
+    idProfessor: number;
     duration: number;
-    questions: number;
+    date: string;
     points: number;
+    
 }
 
 
@@ -25,7 +34,7 @@ function Table() {
     const columns: Column<UserData>[] = React.useMemo(
         () => [
             {
-                Header: 'Subject title',
+                Header: 'Title',
                 accessor: 'subject_title',
             },
             {
@@ -33,8 +42,8 @@ function Table() {
                 accessor: 'duration',
             },
             {
-                Header: 'Questions',
-                accessor: 'questions',
+                Header: 'Date',
+                accessor: 'date',
             },
             {
                 Header: 'Points',
@@ -98,8 +107,84 @@ function Table() {
     );
 }
 
+const SelectCourse: React.FC<{ onSelectCourse: (id: number) => void }> = ({ onSelectCourse }) => {
+    const [courses, setCourses] = useState<Course[]>([]);
+    const [selectedCourseId, setSelectedCourseId] = useState<number | null>(null);
+    const [examData, setExamData] = useState<UserData[]>([]);
+
+    useEffect(() => {
+        const fetchCourses = async () => {
+            const response = await fetch('http://localhost:8192/courses/professor=52');
+            const data = await response.json();
+            setCourses(data);
+        };
+        fetchCourses();
+
+        const savedCourseId = localStorage.getItem('selectedCourseId');
+        if (savedCourseId) {
+            const courseId = parseInt(savedCourseId);
+            setSelectedCourseId(courseId);
+            onSelectCourse(courseId);
+        }
+    }, []);
+
+    useEffect(() => {
+        const fetchExamData = async () => {
+            if (selectedCourseId) {
+                const response = await fetch(`http://localhost:8192/exam/summarise/idCourse=${selectedCourseId}`);
+                const data = await response.json();
+
+                const professorId = 52; // ID-ul profesorului curent
+
+                const filteredData = data.filter((exam: UserData) => exam.idProfessor === professorId);
+                setExamData(filteredData);
+            }
+        };
+        fetchExamData();
+    }, [selectedCourseId]);
+
+    const handleCourseSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const courseId = parseInt(event.target.value);
+        setSelectedCourseId(courseId);
+        onSelectCourse(courseId);
+        localStorage.setItem('selectedCourseId', String(courseId));
+    };
+
+    return (
+        <>
+            <div className={styles['subject-container']}>
+                <select value={selectedCourseId ?? ""} onChange={handleCourseSelect}>
+                    <option value="" disabled hidden>
+                        Courses options
+                    </option>
+                    {courses.map((course) => (
+                        <option
+                            className={styles['subject-options']}
+                            key={course.id}
+                            value={course.id}
+                        >
+                            {course.title}
+                        </option>
+                    ))}
+                </select>
+
+            </div>
+            
+        </>
+    );
+};
+
 
 const Body: React.FC<{}> = () => {
+    const [idC, setIdC] = useState<number | null>(() => {
+        const savedCourseId = localStorage.getItem('selectedCourseId');
+        return savedCourseId ? parseInt(savedCourseId) : null;
+    });
+
+    const handleCourseSelect = (courseId: number) => {
+        setIdC(courseId);
+        localStorage.setItem('selectedCourseId', String(courseId));
+    };
     return (
 
         <>
@@ -112,7 +197,9 @@ const Body: React.FC<{}> = () => {
             </div>
 
             <div className={styles['body--subtitle--container']}>
-                <SelectSubject />
+                <div className={styles['selects']}>
+                    <SelectCourse onSelectCourse={handleCourseSelect} />
+                </div>
             </div>
 
             <div className={styles['body--line']}></div>
